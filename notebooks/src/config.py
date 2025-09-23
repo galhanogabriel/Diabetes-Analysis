6b22070faca4fdc10669f14fbf3eef7d890a3572
Gabriel Galhano <195915235+galhanogabriel@users.noreply.github.com>
2025-09-23 14:33:41 -0300

    basic organization

--- notebooks/src/config.py
+++ notebooks/src/config.py
@@ -6,11 +6,5 @@ PASTA_PROJETO = Path(__file__).resolve().parents[2]
 PASTA_DADOS = PASTA_PROJETO / "dados"
 
 # coloque abaixo o caminho para os arquivos de dados de seu projeto
-DADOS_ORIGINAIS = PASTA_DADOS / "exemplo.csv"
-
-# coloque abaixo o caminho para os arquivos de modelos de seu projeto
-PASTA_MODELOS = PASTA_PROJETO / "modelos"
-
-# coloque abaixo outros caminhos que você julgar necessário
-PASTA_RELATORIOS = PASTA_PROJETO / "relatorios"
-PASTA_IMAGENS = PASTA_RELATORIOS / "imagens"
+DADOS_ORIGINAIS = PASTA_DADOS / "diabetes.csv"
+DADOS_TRATADOS = PASTA_DADOS / "diabetes_tratado.parquet"
