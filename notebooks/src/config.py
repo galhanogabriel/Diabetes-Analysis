dec5c81d80b0e98129a8a3e0d15fd7f00b040738
Gabriel Galhano <195915235+galhanogabriel@users.noreply.github.com>
2025-10-02 15:54:22 -0300

    Adding on config.py

--- notebooks/src/config.py
+++ notebooks/src/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 PROJECT_FOLDER = Path(__file__).resolve().parents[2]
 
-DATA_FOLDER = PROJECT_FOLDER / "dados"
+DATA_FOLDER = PROJECT_FOLDER / "dados" # Data folder
 
 # Place the path to your project's data files below
 ORIGINAL_DATA = DATA_FOLDER / "diabetes.csv"
